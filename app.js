1770dc4d46c227a2b7098cbef0c861bdc6ed3d94
Raymond Luong <raymondluong@Raymonds-MacBook-Pro.local>
2018-08-29 21:04:08 -1000

    all the things for handlebarsgit add -Agit add -A

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
-const express = require('express'); 
+const express = require('express');
 const app = express();
+const bp = require('body-parser');
+const exphbs = require('express-handlebars');
+const Inventory = require('./db/DS_Inventory.js');
+const DS_Inv = new Inventory();
 
-app.get('/', (req,res) => {
-  res.send('Hello DevLeaguers');
+app.use(bp.urlencoded({ extended: true }));
+
+app.engine('.hbs', exphbs({ layout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+// render all items
+app.get('/', (req, res) => {
+  const items = DS_Inv.all();
+  console.log('items', items);
+  res.render('home', { items });
+});
+
+// render out the form
+app.get('/item/new', (req, res) => {
+  res.render('form');
+});
+
+// render out detail
+app.get('/item/:id', (req, res) => {
+  console.log('did it call');
+  const { id } = req.params;
+  const item = DS_Inv.getItemById(id);
+  console.log('item', item);
+  res.render('detail', item);
+});
+
+// add item
+app.post('/item/new', (req, res) => {
+  console.log('req.body', req.body);
+  const item = req.body;
+  DS_Inv.add(item);
+  res.redirect('/');
 });
 
 app.listen(process.env.PORT, () => {
